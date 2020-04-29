4407b36f9e0b6715b349ed2dbccccc0fb780c762
Matthew Nte <Matthewnte1@gmail.com>
2020-04-29 09:31:44 +0100

    add an appbar component

---
+++ components/AppBar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar color="green" dark :clipped-left="clipped" flat fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>
+        <span class="font-weight-bold">9</span>pay
+      </v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items>
+        <v-btn depressed text class="text-none">
+          Login
+        </v-btn>
+        <v-btn text class="text-none">Need Help?</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false
+    }
+  }
+}
+</script>
