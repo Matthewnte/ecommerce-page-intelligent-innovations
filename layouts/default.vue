a6fd311f530e5629d5c5cb555a6e10eb6b545750
Matthew Nte <Matthewnte1@gmail.com>
2020-04-29 09:34:27 +0100

    modified layout.vue file

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,72 +1,23 @@
-<template id="app">
+<template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar color="green" dark :clipped-left="clipped" flat fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>
-        <span class="font-weight-bold">9</span>pay
-      </v-toolbar-title>
-      <v-spacer />
-      <v-toolbar-items>
-        <v-btn flat text class="text-none">Login</v-btn>
-        <v-btn flat text class="text-none">Need Help?</v-btn>
-      </v-toolbar-items>
-    </v-app-bar>
+    <AppBar />
     <v-content>
-      <v-container>
+      <v-container fluid class="pa-0">
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer :fixed="fixed" app>
+    <v-footer>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import AppBar from '@/components/AppBar'
+
 export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false
-    }
+  components: {
+    AppBar
   }
 }
 </script>
