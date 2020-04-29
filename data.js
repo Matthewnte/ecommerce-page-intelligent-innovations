9c794c2941eb5d2e252ad8d09b6056388ff7df99
Matthew Nte <Matthewnte1@gmail.com>
2020-04-29 09:39:50 +0100

    add data.js file

---
+++ data.js
@@ -0,0 +1,44 @@
+export default {
+  merchants: [
+    {
+      title: 'Top western road trips',
+      subtitle: '1,000 miles of wonder',
+      src: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg'
+    },
+    {
+      title: 'Top western road trips',
+      subtitle: '1,000 miles of wonder',
+      src: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg'
+    },
+    {
+      title: 'Top western road trips',
+      subtitle: '1,000 miles of wonder',
+      src: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg'
+    },
+    {
+      title: 'Top western road trips',
+      subtitle: '1,000 miles of wonder',
+      src: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg'
+    },
+    {
+      title: 'Top western road trips',
+      subtitle: '1,000 miles of wonder',
+      src: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg'
+    },
+    {
+      title: 'Top western road trips',
+      subtitle: '1,000 miles of wonder',
+      src: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg'
+    },
+    {
+      title: 'Top western road trips',
+      subtitle: '1,000 miles of wonder',
+      src: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg'
+    },
+    {
+      title: 'Top western road trips',
+      subtitle: '1,000 miles of wonder',
+      src: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg'
+    }
+  ]
+}
