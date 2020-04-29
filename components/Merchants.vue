55d75cb59228182966e14286e3b4880db3502371
Matthew Nte <Matthewnte1@gmail.com>
2020-04-29 09:32:52 +0100

    add a merchant component

---
+++ components/Merchants.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="(merchant, index) in merchants"
+      :key="index"
+      cols="6"
+      sm="4"
+      md="3"
+    >
+      <v-card flat>
+        <v-img :src="merchant.src" height="200px"></v-img>
+        <v-card-title class="body-1">{{ merchant.title }}</v-card-title>
+        <v-card-subtitle>{{ merchant.subtitle }}</v-card-subtitle>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    merchants: Array
+  }
+}
+</script>
