bdf20e2faf90813266064ce498e52b4d6cac94fb
Matthew Nte <Matthewnte1@gmail.com>
2020-04-29 09:39:01 +0100

    modified index.vue file

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
     <Carousel />
+    <v-container>
+      <h2 class="headline font-weight-bold">Merchants</h2>
+      <Merchants :merchants="merchants" />
+    </v-container>
   </div>
 </template>
 
 <script>
-import Carousel from '@/components/carousel'
+import data from '../data'
+import Carousel from '@/components/Carousel'
+import Merchants from '@/components/Merchants'
+
 export default {
   components: {
-    Carousel
-  }
+    Carousel,
+    Merchants
+  },
+  data: () => ({
+    merchants: data.merchants
+  })
 }
 </script>
