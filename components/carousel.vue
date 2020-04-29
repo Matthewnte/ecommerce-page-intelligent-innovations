110e8e4ba894d71f5bb9efcc4b5244f2b6578adc
Matthew Nte <Matthewnte1@gmail.com>
2020-04-29 09:36:11 +0100

    add carousel to page

--- components/carousel.vue
+++ components/carousel.vue
@@ -1,10 +1,19 @@
 <template>
-  <v-carousel cycle height="500" hide-delimiter show-arrows-on-hover>
-    <v-carousel-item
-      v-for="(image, i) in images"
-      :key="i"
-      :src="image.src"
-    ></v-carousel-item>
+  <v-carousel
+    height="450"
+    hide-delimiters
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item v-for="(image, i) in images" :key="i" :src="image.src">
+      <v-container fluid class="fill-height cover-sm">
+        <v-container>
+          <v-btn color="green" x-large depressed>
+            Shop Now<v-icon large right>mdi-chevron-right</v-icon>
+          </v-btn>
+        </v-container>
+      </v-container>
+    </v-carousel-item>
   </v-carousel>
 </template>
 
@@ -13,15 +22,22 @@ export default {
   data: () => ({
     images: [
       {
-        src: '/carousel3.jpg'
-      },
-      {
         src: '/carousel2.jpg'
       },
       {
-        src: '/carousel1.jpg'
+        src: '/carousel1.jpeg'
+      },
+      {
+        src: '/carousel3.jpg'
       }
     ]
   })
 }
 </script>
+
+<style scoped>
+.cover-sm {
+  background: rgba(0, 50, 0, 0.2);
+  height: 70vh;
+}
+</style>
