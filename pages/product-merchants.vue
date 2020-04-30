640ad8d0fa1b05be3d3b75ffa1c9ad4987da08cc
Matthew Nte <Matthewnte1@gmail.com>
2020-04-30 13:31:49 +0100

    add a product page

---
+++ pages/product-merchants.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Categories />
+    <Merchants :merchants="filterServices" />
+  </div>
+</template>
+
+<script>
+import data from '../data'
+import Categories from '@/components/Category'
+import Merchants from '@/components/Merchants'
+
+export default {
+  components: {
+    Categories,
+    Merchants
+  },
+  computed: {
+    filterServices() {
+      return data.merchants.filter((merchant) => {
+        return merchant.category === 'product'
+      })
+    }
+  }
+}
+</script>
